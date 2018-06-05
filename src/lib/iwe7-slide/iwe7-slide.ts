f3f601ac253cbe2c5b3c41e78a98eed71e857dff
iwe7 <admin@meepo.com.cn>
2018-06-05 13:12:00 +0800

    saves

--- src/lib/iwe7-slide/iwe7-slide.ts
+++ src/lib/iwe7-slide/iwe7-slide.ts
@@ -1,9 +1,15 @@
-import { HostBinding, ViewChildren, ViewChild, Input, QueryList, AfterViewInit, ViewEncapsulation, Injectable, Renderer2, AfterContentInit } from '@angular/core';
-import { ElementRef } from '@angular/core';
+import {
+    HostBinding, ViewChildren, ViewChild, Input,
+    QueryList, AfterViewInit, ViewEncapsulation,
+    Injectable, Renderer2, AfterContentInit,
+    ChangeDetectionStrategy
+} from '@angular/core';
+import { ElementRef, Injector } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { BScroll } from '../lib';
 import * as _ from 'lodash';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { Iwe7CoreComponent } from 'iwe7-core';
 @Component({
     selector: 'iwe7-slide',
     templateUrl: 'iwe7-slide.html',
@@ -11,13 +17,13 @@ import { coerceBooleanProperty } from '@angular/cdk/coercion';
     encapsulation: ViewEncapsulation.None,
     host: {
         '[class.slide]': 'true'
-    }
+    },
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentInit {
+export class Iwe7SlideComponent extends Iwe7CoreComponent {
     private slide: any;
     private timer: any;
-    currentPageIndex: number;
-
+    currentPageIndex: number = 0;
     _loop: boolean = true;
     @Input()
     set loop(val: any) {
@@ -26,9 +32,14 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
     get loop() {
         return this._loop;
     }
-    @Input() threshold: number = 0.3;
-    @Input() speed: number = 400;
-    @Input() click: boolean = true;
+    _click: boolean = true;
+    @Input()
+    set click(val: any) {
+        this._click = coerceBooleanProperty(val);
+    }
+    get click() {
+        return this._click;
+    }
     _autoPlay: boolean = true;
     @Input()
     set autoPlay(val: any) {
@@ -37,8 +48,9 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
     get autoPlay() {
         return this._autoPlay;
     }
+    @Input() threshold: number = 0.3;
+    @Input() speed: number = 400;
     @Input() interval: number = 4000;
-
     @Input() height: string = 'auto';
     @ViewChild('group') group: ElementRef;
     @HostBinding('class.scroll-x')
@@ -50,75 +62,70 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
         this._scrollX = coerceBooleanProperty(val);
         this._scrollY = !this._scrollX;
     }
-
     @Input()
     set scrollY(val: any) {
         this._scrollY = coerceBooleanProperty(val);
         this._scrollX = !this._scrollY;
     }
-
     count: number = 0;
-
-    get list() {
-        const items: any[] = [];
-        for (let i = 0; i < this.count; i++) {
-            items.push(i);
-        }
-        return items;
-    }
-    constructor(
-        public ele: ElementRef,
-        public render: Renderer2
-    ) { }
+    list: any[] = [];
 
     get children() {
         return this.group.nativeElement.children;
     }
-
-    ngOnInit() {
-
-    }
-
-    ngAfterContentInit() {
-        this.count = this.children.length;
-        clearTimeout(this.timer);
-        this.currentPageIndex = 0;
-    }
-
-    ngAfterViewInit() {
-        const opt: any = {
-            scrollX: this._scrollX,
-            scrollY: this._scrollY,
-            momentum: false,
-            snap: {
-                loop: this._loop,
-                threshold: this.threshold,
-                speed: this.speed
-            },
-            bounce: false,
-            stopPropagation: true,
-            click: this.click
-        };
-        this.slide = new BScroll(this.ele.nativeElement, opt);
-        this.slide.on('scrollEnd', () => {
-            this._onScrollEnd();
+    constructor(
+        public ele: ElementRef,
+        public render: Renderer2,
+        injector: Injector
+    ) {
+        super(injector);
+        this.runOutsideAngular(() => {
+            this.getCyc('ngAfterContentInit').subscribe(res => {
+                this.count = this.children.length;
+                const items: any[] = [];
+                for (let i = 0; i < this.count; i++) {
+                    items.push(i);
+                }
+                this.list = items;
+                this.currentPageIndex = 0;
+            });
+            this.getCyc('ngAfterViewInit').subscribe(res => {
+                const opt: any = {
+                    scrollX: this._scrollX,
+                    scrollY: this._scrollY,
+                    momentum: false,
+                    snap: {
+                        loop: this._loop,
+                        threshold: this.threshold,
+                        speed: this.speed
+                    },
+                    bounce: false,
+                    stopPropagation: true,
+                    click: this.click
+                };
+                this.slide = new BScroll(this.ele.nativeElement, opt);
+                this.slide.on('scrollEnd', () => {
+                    this.run(() => {
+                        this._onScrollEnd();
+                    });
+                });
+                this.slide.on('touchEnd', () => {
+                    if (this.autoPlay) {
+                        this._play();
+                    }
+                });
+                this.slide.on('beforeScrollStart', () => {
+                    if (this.autoPlay) {
+                        clearTimeout(this.timer);
+                    }
+                });
+                if (this.autoPlay) {
+                    this._play();
+                }
+                this.updateStyle();
+            });
         });
-        this.slide.on('touchEnd', () => {
-            if (this.autoPlay) {
-                this._play();
-            }
-        });
-        this.slide.on('beforeScrollStart', () => {
-            if (this.autoPlay) {
-                clearTimeout(this.timer);
-            }
-        });
-        if (this.autoPlay) {
-            this._play();
-        }
-        setTimeout(() => {
-            this.updateStyle();
-        }, 20);
+
     }
 
     updateStyle() {
@@ -140,7 +147,6 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
         }
         this.render.setStyle(this.group.nativeElement, val, slideWidth * this.children.length + 'px');
         this.render.setStyle(this.group.nativeElement, 'opacity', '1');
-        this.updateDots();
     }
 
     _onScrollEnd() {
@@ -154,6 +160,7 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
         if (this.autoPlay) {
             this._play();
         }
+        this._cd.markForCheck();
     }
 
     refresh() {
@@ -175,6 +182,4 @@ export class Iwe7SlideComponent implements OnInit, AfterViewInit, AfterContentIn
             this.slide.next();
         }, this.interval);
     }
-
-    updateDots() { }
 }
